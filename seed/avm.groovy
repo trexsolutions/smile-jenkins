cbebeef3e457866ed11374a2c0a3fdb53681faae
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-16 20:48:48 -0400

    feat(seed): initial seeds

--- seed/avm.groovy
+++ seed/avm.groovy
@@ -7,28 +7,94 @@ folder('smile/avm') {
 	description('Folder for the Account Vending Machine')
 }
 folder('smile/lz') {
-	displayName('AVM')
+	displayName('LZ')
 	description('Folder for Landing Zones')
 }
 
 pipelineJob('smile/avm/create') {
 	displayName('Smile AVM Account Create')
 	description('Creates a new Account in the Organization')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/avm/account/create.groovy')
+        }
+	}
 }
 pipelineJob('smile/avm/delete') {
 	displayName('Smile AVM Account Delete')
 	description('Removes account from the Organization')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/avm/account/delete.groovy')
+        }
+	}
 }
+
 pipelineJob('smile/avm/provision') {
 	displayName('Smile AVM Account Provision')
 	description('Provisions an account')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/avm/account/provision.groovy')
+        }
+	}
 }
 
 pipelineJob('smile/lz/create') {
 	displayName('Smile Landing Zone Create')
 	description('Creates a Landing Zone')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/lz/create.groovy')
+        }
+	}
 }
 pipelineJob('smile/lz/delete') {
 	displayName('Smile Landing Zone Delete')
 	description('Deletes a Landing Zone')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/lz/delete.groovy')
+        }
+	}
 }
