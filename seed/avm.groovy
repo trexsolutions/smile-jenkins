89d1664c759fbb0cf01423c649fb2f817e9fecd3
Philip M. Gollucci <philip.gollucci@trexsolutionsllc.com>
2020-09-16 20:35:18 -0400

    feat(seed): adds avm and lz structure (#7)

--- seed/avm.groovy
+++ seed/avm.groovy
@@ -1,14 +1,34 @@
 folder('smile') {
-  displayName('Smile')
-  description('Folder for Smile')
+	displayName('Smile')
+	description('Folder for Smile')
 }
-
 folder('smile/avm') {
-  displayName('AVM')
-  description('Folder for Account Vending Machine')
+	displayName('AVM')
+	description('Folder for the Account Vending Machine')
+}
+folder('smile/lz') {
+	displayName('AVM')
+	description('Folder for Landing Zones')
 }
 
 pipelineJob('smile/avm/create') {
 	displayName('Smile AVM Account Create')
-    description('Creates a new Account in the Organization')
+	description('Creates a new Account in the Organization')
+}
+pipelineJob('smile/avm/delete') {
+	displayName('Smile AVM Account Delete')
+	description('Removes account from the Organization')
+}
+pipelineJob('smile/avm/provision') {
+	displayName('Smile AVM Account Provision')
+	description('Provisions an account')
+}
+
+pipelineJob('smile/lz/create') {
+	displayName('Smile Landing Zone Create')
+	description('Creates a Landing Zone')
+}
+pipelineJob('smile/lz/delete') {
+	displayName('Smile Landing Zone Delete')
+	description('Deletes a Landing Zone')
 }
