2458fbbaf27dfffdd68b5ff0c5427cfff383a480
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-16 20:13:12 -0400

    feat(seed): folders

--- seed/avm.groovy
+++ seed/avm.groovy
@@ -1,3 +1,14 @@
-pipelineJob('Smile/AVM/Create') {
+folder('smile') {
+  displayName('Smile')
+  description('Folder for Smile')
+}
+
+folder('smile/avm') {
+  displayName('AVM')
+  description('Folder for Account Vending Machine')
+}
+
+pipelineJob('smile/avm/create') {
 	displayName('Smile AVM Account Create')
+    description('Creates a new Account in the Organization')
 }
