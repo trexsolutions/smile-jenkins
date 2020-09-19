a53cebd1bfb9b18389c964db22ed57651ea83144
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-09-19 15:01:52 -0400

    Revert "chore(debug): move checkout (#10)"
    This reverts commit 37a8bf99d1d166218c601b05578bbbe5b941c683.

--- seed/avm.groovy
+++ seed/avm.groovy
@@ -2,7 +2,6 @@ folder('smile') {
 	displayName('Smile')
 	description('Folder for Smile')
 }
-/*
 folder('smile/avm') {
 	displayName('AVM')
 	description('Folder for the Account Vending Machine')
@@ -11,16 +10,17 @@ folder('smile/lz') {
 	displayName('LZ')
 	description('Folder for Landing Zones')
 }
-*/
 folder('smile/container') {
 	displayName('Container')
 	description('Folder for Landing Zones')
 }
 
-pipelineJob('smile/container/build_tag_push') {
-	displayName('Smile Container Build, Tag, & Push')
-	description('Builds Dockerfile & Pushes to ECR')
+pipelineJob('smile/avm/create') {
+	displayName('Smile AVM Account Create')
+	description('Creates a new Account in the Organization')
 	definition {
+    	cpsScm {
+        	scm {
             	git {
                 	remote {
                 		url('https://github.com/trexsolutions/smile-cdk.git')
@@ -28,7 +28,95 @@ pipelineJob('smile/container/build_tag_push') {
                     branches('*/master')
                 }
             }
-            scriptPath('pipelines/container/build_tag_push.groovy')
+            scriptPath('pipelines/avm/account/create.groovy')
+        }
+	}
+}
+pipelineJob('smile/avm/delete') {
+	displayName('Smile AVM Account Delete')
+	description('Removes account from the Organization')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/avm/account/delete.groovy')
+        }
+	}
+}
+
+pipelineJob('smile/avm/provision') {
+	displayName('Smile AVM Account Provision')
+	description('Provisions an account')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/avm/account/provision.groovy')
+        }
+	}
+}
+
+pipelineJob('smile/lz/create') {
+	displayName('Smile Landing Zone Create')
+	description('Creates a Landing Zone')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/lz/create.groovy')
+        }
+	}
+}
+pipelineJob('smile/lz/delete') {
+	displayName('Smile Landing Zone Delete')
+	description('Deletes a Landing Zone')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/lz/delete.groovy')
+        }
+	}
+}
+
+pipelineJob('smile/container/build_publish') {
+	displayName('Smile Container Build & Publish')
+	description('Builds Dockerfile & Publishes to ECR')
+	definition {
+    	cpsScm {
+        	scm {
+            	git {
+                	remote {
+                		url('https://github.com/trexsolutions/smile-cdk.git')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/container/build_publish.groovy')
         }
 	}
 }
