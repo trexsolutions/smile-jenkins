94885eecd9ca6e80bd113393b9611ebbe0184c28
Philip M. Gollucci <philip.gollucci@trexsolutionsllc.com>
2020-11-16 10:22:37 -0500

    fix(whitespace): jenkins doesn't do tabs (#22)
    WARNING o.e.j.s.h.ContextHandler$Context#log:
    	Error while serving http://localhost:8080/configuration-as-code/replace
    	while scanning for the next token found character '\t(TAB)' that cannot start any token.
    	(Do not use \t(TAB) for indentation) in
    	https://raw.githubusercontent.com/trexsolutions/smile-jenkins/master/smile.groovy, line 5, column 1:
            description('Smile Folder')
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- smile.groovy
+++ smile.groovy
@@ -2,113 +2,95 @@ String basePath = 'Smile'
 String repo = 'https://github.com/trexsolutions/smile-cdk'
 
 folder(basePath) {
-	description('Smile Folder')
+  description('Smile Folder')
 }
 
 pipelineJob("$basePath/smile-builder") {
-	description('Builds smile-builder image and pushes to ecr')
-	definition {
-    	cpsScm {
-        	scm {
-            	git {
-                	remote {
-						credentials('jenkins-github')
-                		url(repo)
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('pipelines/smile-builder/Jenkinsfile')
+  description('Builds smile-builder image and pushes to ecr')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+		    credentials('jenkins-github')
+            url(repo)
+          }
+          branches('*/master')
         }
-	}
+      }
+      scriptPath('pipelines/smile-builder/Jenkinsfile')
+    }
+  }
 }
 
 pipelineJob("$basePath/smile-provisioner") {
-	description('Builds smile-provisioner and pushes to ecr')
-	definition {
-    	cpsScm {
-        	scm {
-            	git {
-                	remote {
-						credentials('jenkins-github')
-                		url(repo)
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('pipelines/smile-provisioner/Jenkinsfile')
+  description('Builds smile-provisioner and pushes to ecr')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+		    credentials('jenkins-github')
+            url(repo)
+          }
+          branches('*/master')
         }
-	}
-}
-
-pipelineJob("$basePath/smile-provision") {
-	description('Provisions the account')
-	definition {
-    	cpsScm {
-        	scm {
-            	git {
-                	remote {
-						credentials('jenkins-github')
-                		url(repo)
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('pipelines/smile-provision/Jenkinsfile')
-        }
-	}
+      }
+      scriptPath('pipelines/smile-provisioner/Jenkinsfile')
+    }
+  }
 }
 
 pipelineJob("$basePath/smile-avm-account-create") {
-	description('Creates an AWS Organization Account')
-	definition {
-    	cpsScm {
-        	scm {
-            	git {
-                	remote {
-						credentials('jenkins-github')
-                		url(repo)
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('pipelines/smile-avm/Jenkinsfile.create')
+  description('Creates an AWS Organization Account')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+		    credentials('jenkins-github')
+            url(repo)
+          }
+          branches('*/master')
         }
-	}
+      }
+      scriptPath('pipelines/smile-avm/Jenkinsfile.create')
+    }
+  }
 }
 
 pipelineJob("$basePath/smile-avm-account-provision") {
-	description('Provisions an AWS Organization Account')
-	definition {
-    	cpsScm {
-        	scm {
-            	git {
-                	remote {
-						credentials('jenkins-github')
-                		url(repo)
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('pipelines/smile-avm/Jenkinsfile.provision')
+  description('Provisions an AWS Organization Account')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+		    credentials('jenkins-github')
+            url(repo)
+          }
+          branches('*/master')
         }
-	}
+      }
+      scriptPath('pipelines/smile-avm/Jenkinsfile.provision')
+    }
+  }
 }
 
 pipelineJob("$basePath/smile-avm-account-delete") {
-	description('Deletes an AWS Organization Account')
-	definition {
-    	cpsScm {
-        	scm {
-            	git {
-                	remote {
-						credentials('jenkins-github')
-                		url(repo)
-                    }
-                    branches('*/master')
-                }
-            }
-            scriptPath('pipelines/smile-avm/Jenkinsfile.delete')
+  description('Deletes an AWS Organization Account')
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+		    credentials('jenkins-github')
+            url(repo)
+          }
+          branches('*/master')
         }
-	}
+      }
+      scriptPath('pipelines/smile-avm/Jenkinsfile.delete')
+    }
+  }
 }
