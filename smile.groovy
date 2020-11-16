5f69b9943d38b28290c7cc4bb3ccfe7d2755a2d3
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-11-16 10:22:15 -0500

    bring out the hammer

--- smile.groovy
+++ smile.groovy
@@ -8,89 +8,107 @@ folder(basePath) {
 pipelineJob("$basePath/smile-builder") {
   description('Builds smile-builder image and pushes to ecr')
   definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-		    credentials('jenkins-github')
-            url(repo)
-          }
-          branches('*/master')
+      cpsScm {
+          scm {
+              git {
+                  remote {
+            credentials('jenkins-github')
+                    url(repo)
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/smile-builder/Jenkinsfile')
         }
-      }
-      scriptPath('pipelines/smile-builder/Jenkinsfile')
-    }
   }
 }
 
 pipelineJob("$basePath/smile-provisioner") {
   description('Builds smile-provisioner and pushes to ecr')
   definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-		    credentials('jenkins-github')
-            url(repo)
-          }
-          branches('*/master')
+      cpsScm {
+          scm {
+              git {
+                  remote {
+            credentials('jenkins-github')
+                    url(repo)
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/smile-provisioner/Jenkinsfile')
         }
-      }
-      scriptPath('pipelines/smile-provisioner/Jenkinsfile')
-    }
+  }
+}
+
+pipelineJob("$basePath/smile-provision") {
+  description('Provisions the account')
+  definition {
+      cpsScm {
+          scm {
+              git {
+                  remote {
+            credentials('jenkins-github')
+                    url(repo)
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/smile-provision/Jenkinsfile')
+        }
   }
 }
 
 pipelineJob("$basePath/smile-avm-account-create") {
   description('Creates an AWS Organization Account')
   definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-		    credentials('jenkins-github')
-            url(repo)
-          }
-          branches('*/master')
+      cpsScm {
+          scm {
+              git {
+                  remote {
+            credentials('jenkins-github')
+                    url(repo)
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/smile-avm/Jenkinsfile.create')
         }
-      }
-      scriptPath('pipelines/smile-avm/Jenkinsfile.create')
-    }
   }
 }
 
 pipelineJob("$basePath/smile-avm-account-provision") {
   description('Provisions an AWS Organization Account')
   definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-		    credentials('jenkins-github')
-            url(repo)
-          }
-          branches('*/master')
+      cpsScm {
+          scm {
+              git {
+                  remote {
+            credentials('jenkins-github')
+                    url(repo)
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/smile-avm/Jenkinsfile.provision')
         }
-      }
-      scriptPath('pipelines/smile-avm/Jenkinsfile.provision')
-    }
   }
 }
 
 pipelineJob("$basePath/smile-avm-account-delete") {
   description('Deletes an AWS Organization Account')
   definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-		    credentials('jenkins-github')
-            url(repo)
-          }
-          branches('*/master')
+      cpsScm {
+          scm {
+              git {
+                  remote {
+            credentials('jenkins-github')
+                    url(repo)
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('pipelines/smile-avm/Jenkinsfile.delete')
         }
-      }
-      scriptPath('pipelines/smile-avm/Jenkinsfile.delete')
-    }
   }
 }
