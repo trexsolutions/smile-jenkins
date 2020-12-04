2cadebbb78496f118bc2594ee18e5ed5ae5fa945
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-12-04 10:09:00 -0500

    chore(deps): bumps projen from 0.3.169 to 0.6.3; bumps aws-cdk from 1.73.0 to 1.76.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,52 +1,64 @@
-const { AwsCdkTypeScriptApp, Semver } = require('projen');
+const { AwsCdkTypeScriptApp } = require('projen');
 
+/**
+ * An AWS CDK TypeScript Application
+ * Applications unlike Constructs can be directly deployed
+ */
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: "1.73.0",
-  name: "smile-jenkins",
-  description: 'Jenkins Seed Files',
-  authorOrganization: 'trexsolutions',
+  /**
+   * The master cdk version
+   * https://github.com/projen/projen/blob/master/API.md#struct-awscdktypescriptappoptions--
+   */
+  cdkVersion: '1.76.0',
+  name: 'smile-jenkins',
+  description: 'Seed Jobs',
+  authorOrganization: 'trexsolutions', // This must match the NPMJS organization
   authorName: 'T-Rex Centers of Excellence (CoE)',
   authorEmail: 'coeteam@trexsolutionsllc.com',
-  repository: 'https://github.com/trexsolutions/smile-cdk.git',
+  repository: 'https://github.com/trexsolutions/smile-jenkins.git',
   keywords: [
-    'landing zones',
-    'cdk',
-    'security',
-    'account vending machine',
-    'devsecops',
-    'jenkins'
+    'jenkins',
   ],
 
+  // XXX: need to add this secret in GitHub
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 });
 
-project.addScriptCommand('docgen', 'yarn run doc');
-project.addScriptCommand('docgen', 'yarn run viz');
+project.gitignore.exclude('.tools');
 
-project.mergify.addRule({
+/**
+ * Label pgollucci PR as contribution/core which is common in OSS
+ */
+project.github.addMergifyRules({
   name: 'Label core contributions',
   actions: {
     label: {
-      add: ['contribution/core']
-    }
+      add: ['contribution/core'],
+    },
   },
   conditions: [
     'author~=^(pgollucci)$',
-    'label!=contribution/core'
+    'label!=contribution/core',
   ],
 });
 
-project.mergify.addRule({
+/**
+ * Until more than one person, auto-merge core PRs if CI passes
+ */
+project.github.addMergifyRules({
   name: 'Label auto-merge for core',
   actions: {
     label: {
-      add: ['auto-merge']
-    }
+      add: ['auto-merge'],
+    },
   },
   conditions: [
     'label=contribution/core',
-    'label!=auto-merge'
+    'label!=auto-merge',
   ],
 });
 
+/**
+ * Write it to the directory
+ */
 project.synth();
